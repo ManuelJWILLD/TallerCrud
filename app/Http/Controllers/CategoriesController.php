1ef63ed385dab23e139c9bdb2c01230260e4ee6e
ManuelJWILLD <marestrepoq@unal.edu.co>
2024-12-12 15:59:26 -0500

    Vista, migraciones, controlador, modelo

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Categories;
 
 class CategoriesController extends Controller
 {
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -10,7 +10,7 @@
     <h1>Categories</h1>
 
     <div>
-        <form action="{{route('categories.store')}}" method="post">
+        <form action="{{-- {{route('categories.store')}}--}}" method="post">
                 @csrf
             
                 <label for="name">Name</label>
