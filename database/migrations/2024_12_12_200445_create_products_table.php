0c1d5cb660c34db37c2ed4482916e24fbcd2cab8
ManuelJWILLD <marestrepoq@unal.edu.co>
2024-12-12 15:25:48 -0500

    migraciones completas

--- database/migrations/2024_12_12_200445_create_products_table.php
+++ database/migrations/2024_12_12_200445_create_products_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
             $table->string('name');
-            $table->int('price');
-            $table->int('stock');
-            $table->foreing('category_id')->references('id')->on('category');
+            $table->integer('price');
+            $table->integer('stock');
         });
     }
 
