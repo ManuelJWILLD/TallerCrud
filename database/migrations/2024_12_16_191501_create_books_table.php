1945db5077336cacb1bf271dbd5643bac9adc177
wwcarlos <carmorenoc@unal.edu.co>
2024-12-16 14:44:29 -0500

    se agregan los datos a la tabla libro

--- database/migrations/2024_12_16_191501_create_books_table.php
+++ database/migrations/2024_12_16_191501_create_books_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('book', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('tittle');
+            $table->date('pubdate');
+            $table->string('genre');
+    
+
         });
     }
 
